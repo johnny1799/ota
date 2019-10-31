221949b444d602a9cde886df7739a2a45e4fe6d9
liqiang0177 <liqiang0177@a-su.com.cn>
2019-10-31 17:04:56 +0800

    加载时网络请求不在主线程

--- app/src/main/java/com/asu/ota/MainActivity.java
+++ app/src/main/java/com/asu/ota/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +18,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.asu.ota.database.DatabaseHelper;
+import com.asu.ota.http.Request;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //网络连接不能放在主线程
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //设置为竖屏
@@ -60,22 +70,32 @@ public class MainActivity extends AppCompatActivity {
                 }else if(TextUtils.isEmpty(psw)){
                     Toast.makeText(MainActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
                     return;
-                }else if("admin".equals(userName)&&"admin".equals(psw)){
-                    //一致登录成功
-                    Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
-                    //登录成功后关闭此页面进入主页
-                    Intent data=new Intent();
-                    //datad.putExtra( ); name , value ;
-                    data.putExtra("isLogin",true);
-                    //RESULT_OK为Activity系统常量，状态码为-1
-                    // 表示此页面下的内容操作成功将data返回到上一页面，如果是用back返回过去的则不存在用setResult传递data值
-                    setResult(RESULT_OK,data);
-                    //跳转到主界面，登录成功的状态传递到 MainActivity 中
-                    startActivity(new Intent(MainActivity.this, ProductActivity.class));
-                    return;
-                }else {
-                    Toast.makeText(MainActivity.this, "输入的用户名或密码不争取", Toast.LENGTH_SHORT).show();
-                    return;
+                }else{
+                    try {
+                        String url = "http://192.168.11.106:8089/home/login";
+                        String param = "username="+userName+"&password="+psw;
+                        String result = new Request().sendPost(url,param);
+                        JSONObject jo = new JSONObject(new String(result));
+                        Integer code = (Integer)jo.get("code");
+                        if(code == 0){
+                            //一致登录成功
+                            Toast.makeText(MainActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                            //登录成功后关闭此页面进入主页
+                            Intent data=new Intent();
+                            //datad.putExtra( ); name , value ;
+                            data.putExtra("isLogin",true);
+                            //RESULT_OK为Activity系统常量，状态码为-1
+                            // 表示此页面下的内容操作成功将data返回到上一页面，如果是用back返回过去的则不存在用setResult传递data值
+                            setResult(RESULT_OK,data);
+                            //跳转到主界面，登录成功的状态传递到 MainActivity 中
+                            startActivity(new Intent(MainActivity.this, ProductActivity.class));
+                            return;
+                        }else{
+                            Toast.makeText(MainActivity.this, "账户或密码错误", Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (Exception e){
+                        Log.e("login",e.getMessage());
+                    }
                 }
             }
         });
